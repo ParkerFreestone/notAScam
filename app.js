dfbb476c74fc9be84e96e29dd9b37e56077cba89
ParkerFreestone <52507320+ParkerFreestone@users.noreply.github.com>
2020-11-02 15:27:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const text = document.getElementById("text-result")
 
 function getRandomInt() {
 
-    var number = Math.floor(Math.random() * Math.floor(4));
+    var number = Math.floor(Math.random() * Math.floor(2));
     number += 1;
 
     if (number == 1) {
-        text.innerHTML = ("Go hang with the main homies :)")
+        text.innerHTML = ("Break up with him")
     } else if (number == 2) {
-        text.innerHTML = ("Stay home like a lamo and watch criminal minds.")
-    } else if (number == 3) {
-        text.innerHTML = ("Go to the 23 year old guys cabin 🤔")
-    } else if (number == 4) {
-        text.innerHTML = ("Watch Stars wif me? :)")
-    }
+        text.innerHTML = ("Stay with him")
+    } 
 
 }
 
